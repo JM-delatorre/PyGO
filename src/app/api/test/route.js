ae91444734296f2519673a5a37528b430dd2efb7
Juan Manuel De La Torre Sanchez <jdela@unal.edu.co>
2023-06-05 00:27:16 -0500

    deleting all antlr files

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,6 +1,6 @@
 import {headers} from 'next/headers';
 
-export async function GET(request){
+export async function POST(request){
     const headersList = headers();
     const referer = headersList.get('referer');
     return new Response('Hello, Next js!',{
